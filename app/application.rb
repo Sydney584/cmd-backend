fd2aa7d99a1d5bb080fc0945d9498139d222676d
sydney <sydneychase5841@gmail.com>
2021-08-10 23:28:46 -0400

    created index,create, show, update, delete for user

--- app/application.rb
+++ app/application.rb
@@ -4,18 +4,55 @@ class Application
     res = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path == '/users' && req.get?
-      users = User.all
-      return [
-        200, 
-        { 'Content-Type' => 'application/json' }, 
-        [ users.to_json ]
-      ]
+    # EXERCISER ROUTES
+   
 
-    elsif req.path.match(/users/) && req.get?
+    
+    # Exerciser Index
+    if req.path == ('/quotes') && req.get?
+      return [200, {'Content-Type' => 'application/json'}, [Quote.all.to_json]]
+    end
+
+    if req.path == ('/users') && req.get?
+      return [200, {'Content-Type' => 'application/json'}, [User.all.to_json]]
+    end
+
+    if req.path == ('/mytodos') && req.get?
+      return [200, {'Content-Type' => 'application/json'}, [Mytodo.all.to_json]]
+    end
+
+    if req.path == ('/mynotes') && req.get?
+      return [200, {'Content-Type' => 'application/json'}, [Mynote.all.to_json]]
+    end
+
+    if req.path == ('/mywins') && req.get?
+      return [200, {'Content-Type' => 'application/json'}, [Mywin.all.to_json]]
+    end
+
+    if req.path == ('/unitodos') && req.get?
+      return [200, {'Content-Type' => 'application/json'}, [Unitodo.all.to_json]]
+    end
+
+    # Exerciser Creater
+    if req.path == ('/users') && req.post?
+      body = JSON.parse(req.body.read)
+      user = User.create(body)
+      return [201, {'Content-Type' => 'application/json'}, [user.to_json]]
+    end
+
+    if req.path == ('/mytodos') && req.post?
+      body = JSON.parse(req.body.read)
+      mytodo = Mytodo.create(body)
+      return [201, {'Content-Type' => 'application/json'}, [mytodo.to_json]]
+    end
+
+    # Exerciser Show
+   
+
+    if req.path.match('/users/') && req.get?
       id = req.path.split("/")[2]
       user = User.find_by(id: id)
-      if user
+  
         unitodos = user.unitodos
         mynotes = user.mynotes
         mywins = user.mywins
@@ -32,13 +69,36 @@ class Application
         { 'Content-Type' => 'application/json' }, 
         [ user_res.to_json ]
       ]
-    else
-       return [
-        204, 
-        { 'Content-Type' => 'application/json' }, 
-        [ { error: 'user not found'}.to_json]
-      ]
-      end
+    end
+
+    
+    # Exerciser Update
+    if req.path.match('/users/') && req.put?
+      id = req.path.split('/')[2]
+      body = JSON.parse(req.body.read)
+      user = User.find(id)
+      user.update(body)
+      
+      return [202, {'Content-Type' => 'application/json'}, [user.to_json]]
+    end
+
+
+    # Exerciser Delete
+
+    if req.path.match('/users/') && req.delete?
+      id = req.path.split('/')[2]
+        user = User.find(id)
+        user.destroy
+        return [200, {'Content-Type' => 'application/json'}, [{message: "User Destroyed"}.to_json]]
+      
+    end
+
+     
+     
+      
+    # static route to test rack
+    if req.path.match(/test/) 
+      return [200, { 'Content-Type' => 'application/json' }, [ {:message => "test response!"}.to_json ]]
 
     else
       res.write "Path Not Found"
@@ -48,4 +108,6 @@ class Application
     res.finish
   end
 
+ 
 end
+
